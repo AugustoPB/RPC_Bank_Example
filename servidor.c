712de0eb88259f71f1517d8bf2caf5b0efe7a5c6
Augusto <tinho.pb@hotmail.com>
2017-09-18 21:03:09 -0300

    modified servidor

--- servidor.c
+++ servidor.c
@@ -1,7 +1,7 @@
 #include "administration.h"
 #include "const.h"
 
-
+AGENCY agencies[5];
 
 //double *obtem_nota_1_svc(struct simple *test)
 //{
@@ -22,15 +22,43 @@
 //			return &(notas[i].nota);
 //	return &erro;
 //}
-
-double *obtem_nota_1_svc(struct simple *test)
+int create_account(MANAGEMENT *operation)
 {
-	static double a = 4;
+	ACCOUNT new_account;
+	new_account.first_name = operation->first_name;
+	new_account.last_name = operation->last_name;
+	new_account.cpf = operation->cpf;
+	new_account.agency = operation->agency;
+	new_account.balance = 0;
+
+
+	if(agencies[operation->agency-1].agency != operation->agency)
+		return -2;
+
+	int iterator;
+	for(iterator = 0; iterator < MAX_ACCOUNTS_NUMBER; iterator++)
+	{
+		if(agencies[operation->agency-1].accounts[iterator].account_number == 0)
+		{
+			new_account.account_number = iterator+1;
+			agencies[operation->agency-1].accounts[iterator] = new_account;
+			return new_account.account_number;
+		}
+	}
+	return -1;
+}
+
+double *obtem_nota_1_svc(MANAGEMENT *operation)
+{
+	static double a;
+
+	a = create_account(operation);
 	return &a;
 }
 
 int main()
 {
+	agencies[0].agency = 1;
 	if	(registerrpc(BANKPROG,BANKVERS,MANAGEMENT_OP,obtem_nota_1_svc,
 			     (xdrproc_t)xdr_management,(xdrproc_t)xdr_double ) == -1)  {
 		fprintf(stderr,"Erro em registerrpc()!\n");
